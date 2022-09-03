35be4cdddc2824df9680fcec361f5c3415a5c02e
rzexin <rzexin@gmail.com>
2022-09-03 21:31:49 +0800

    up

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -7,6 +7,16 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
+	type KittyIndex = u32;
+
+	#[pallet::type_value]
+	pub fn GetDefaultValue() -> KittyIndex {
+		0_u32
+	}
+
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
+	pub struct Kitty(pub [u8; 16]);
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
@@ -15,6 +25,19 @@ pub mod pallet {
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	#[pallet::getter(fn next_kitty_id)]
+	pub type NextKittyId<T> = StorageValue<_, KittyIndex, ValueQuery, GetDefaultValue>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitties)]
+	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyIndex, Kitty>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitty_owner)]
+	//pub type Kitties<T: Config> = StorageMap<_, Blake2_128Concat, KittyIndex, T::AccountId>;
+	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyIndex, T::AccountId>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -26,5 +49,25 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {}
 
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		fn random_value(sender: &T::AccountId) -> [u8; 16] {
+			let payload = (
+				T::Randomness::random_seed(),
+				&sender,
+				<frame_system::Pallet<T>>::extrinsic_index(),
+			);
+		}
+
+		fn get_next_id() -> Result<KittyIndex, ()> {
+			match Self::next_kitty_id() {
+				KittyIndex::MAX => Err(()),
+				val => Ok(val),
+			}
+		}
+
+        fn get_kitty(kitty_id: KittyIndex) -> Result<Kitty, ()> {
+            match Self::Kitties(kitty_id),
+            None => Err(()),
+        }
+	}
 }
